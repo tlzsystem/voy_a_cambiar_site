651e8ac1ec6603961e11f7159d8c1b46af299d0d
Samuel Toloza <tlz.system@gmail.com>
2026-03-06 23:42:02 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-typescript: {
+  output: 'export',
+  typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
