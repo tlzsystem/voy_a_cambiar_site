e452b8525f11289ad533209d8df356e54bb883d3
Samuel Toloza <tlz.system@gmail.com>
2026-03-06 23:21:11 -0300

    delete theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
